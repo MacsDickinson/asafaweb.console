2aafc1d9385808630f9199d8c6676b36f13b9af9
Macs Dickinson <macsyd@hotmail.co.uk>
2013-05-28 16:33:23 +0100

    Added status logic

--- src/asafaweb.tests/ProgramTestFixture.cs
+++ src/asafaweb.tests/ProgramTestFixture.cs
@@ -1,10 +1,24 @@
 ﻿using NUnit.Framework;
+using asafaweb.console.Enums;
+using asafaweb.console.Logic;
 
 namespace asafaweb.tests
 {
     [TestFixture]
     public class ProgramTestFixture
     {
-
+        [TestCase("Pass", AsafaResult.Pass)]
+        [TestCase("Warning", AsafaResult.Warning)]
+        [TestCase("Fail", AsafaResult.Fail)]
+        [TestCase("NotTested", AsafaResult.NotTested)]
+        [TestCase("XXXXXX", AsafaResult.NotTested)]
+        public void StatusLogic_GetStatus_ReturnsExpectedStatus(string input, AsafaResult expectedStatus)
+        {
+            // Arrange
+            // Act
+            AsafaResult actualStatus = StatusLogic.GetStatus(input);
+            // Assert
+            Assert.That(actualStatus, Is.EqualTo(expectedStatus));
+        }
     }
 }
